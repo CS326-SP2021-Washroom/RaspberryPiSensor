cc230f27a78f9c5689e35780cf3ab3dc654ffe1b
Josh Ridder <71297671+joshridder@users.noreply.github.com>
2021-05-05 13:20:32 -0400

    Comments for sensorVersion2

--- sensorVersion2.py
+++ sensorVersion2.py
@@ -1,27 +1,44 @@
+# Usage: python sensorVersion2.py -l -b -c -u -p
+#    -l: Location (Dorm/apartment name)
+#    -b: Broker address
+#    -c: Broker cerificates (optional)
+#    -u: Broker username (optional)
+#    -p: Broker password (optional)
+# This programs monitors 2 washers and 2 dryers.
+# Uses cs326/washroom/Location/request for requests
+# Uses cs326/washroom/Location for output
+# Josh Ridder for cs326
+
 import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
-import time
-from threading import Thread, Lock
-
-##### Change this string depending on the dorm the Pi resides in
-LOCATION = "Kappa"
-##### Change this string depending on the dorm the Pi resides in
+import sys
+import argparse
+from help_string import HELP_STRING
 
 HOUR = 60
 HALF_HOUR = 30
-BROKER = 'iot.cs.calvin.edu'
 PORT = 8883
 QOS = 2
-USERNAME = 'NA'
-PASSWORD = 'NA'
-CERTS = '/etc/ssl/certs/ca-certificates.crt'
 WASHER1_PIN = 12
 WASHER2_PIN = 16
 DRYER1_PIN = 23
 DRYER2_PIN = 25
-#CYCLE_START_COUNT = 2
-CYCLE_TIMEOUT_COUNT = 150
-INTERVAL_SLEEP_TIMER = 1
+
+# parse arguments given to program 
+parser = argparse.ArgumentParser()
+parser.add_argument("-l", "--location", help="Dorm/Apartment name")
+parser.add_argument("-b", "--broker", help="MQTT Broker")
+parser.add_argument("-c", "--certs", help="Broker Certificates (optional)")
+parser.add_argument("-u", "--username", help="Broker Username (optional)")
+parser.add_argument("-p", "--password", help="Broker Password (optional)")
+parser.add_argument("-h", "--help", help="Show help text")
+args = parser.parse_args()
+
+# print HELP_STRING and quit if not provided with necessary flags,
+# or if called with help flag
+if not args.location or not args.broker or args.help:
+    print(HELP_STRING)
+    sys.exit()
 
 def on_connect(userdata, rc, *extra_params):
     ''' Provides feedback upon connecting to MQTT. '''
@@ -40,7 +57,7 @@ def on_message(client, userdata, message):
     washer2_status = (time.localtime().tm_hour) * HOUR + (time.localtime().tm_min) < timeWasher2 + HALF_HOUR
 
     print("washer1 is ", washer1_status, " washer2 is ", washer2_status, " dryer1 is ", dryer1_status, " dryer2 is ", dryer2_status)
-    client.publish("cs326/washroom/" + LOCATION, payload=str(int(washer1_status)) + str(int(washer2_status)) + str(int(dryer1_status)) + str(int(dryer2_status)) )
+    client.publish("cs326/washroom/" + args.location, payload=str(int(washer1_status)) + str(int(washer2_status)) + str(int(dryer1_status)) + str(int(dryer2_status)) )
 
 # initialize pins and washer status
 GPIO.setmode(GPIO.BCM)
@@ -87,14 +104,18 @@ GPIO.add_event_detect(WASHER2_PIN, GPIO.RISING, callback=watcher, bouncetime=250
 GPIO.add_event_detect(DRYER1_PIN, GPIO.RISING, callback=watcher, bouncetime=250)
 GPIO.add_event_detect(DRYER2_PIN, GPIO.RISING, callback=watcher, bouncetime=250)
 
+# set username and password if provided
+if args.username and args.password:
+    client.username_pw_set(args.username, password=args.password)
+if args.certs:
+    client.tls_set(args.certs)
+
 # intialize mqtt client and mutex
 client = mqtt.Client()
-client.username_pw_set(USERNAME, password=PASSWORD)
-client.tls_set(CERTS)
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect(BROKER, PORT, 60)
-client.subscribe("cs326/washroom/" + LOCATION + "/request", qos=QOS)
+client.connect(args.broker, PORT, 60)
+client.subscribe("cs326/washroom/" + args.location + "/request", qos=QOS)
 client_mutex = Lock()
 
 try:
